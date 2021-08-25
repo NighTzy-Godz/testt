7cddbdd713bdcd950093860e142c3b3c10edbfff
Aser James <ajhubero16@gmail.com>
2021-08-25 22:20:52 +0800

    test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn testwebsite --log-file -
--- testwebsite/testwebsite/settings.py
+++ testwebsite/testwebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o!gl6bxend8t(svlu0jd$t0)o06a@&kehx4172c_2@7!3f-%m='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerzone.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'base',
 ]
 
 MIDDLEWARE = [
